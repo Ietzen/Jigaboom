baa9591695f8a4b6c1e4dda607c62c547a97092b
Kolev,Nikolay N <n.kolev@student.fontys.nl>
2019-10-26 12:50:50 +0200

    grid

--- Jigaboom/Jigaboom/Grid/Grid.cs
+++ Jigaboom/Jigaboom/Grid/Grid.cs
@@ -23,7 +23,26 @@ namespace Jigaboom.Grid
 
         List<Pebble> getAdjasentPebbles(Pebble pebble)
         {
-            if()
+            List<Pebble> returnedList = new List<Pebble>();
+
+            if (getPebbleByXY(pebble.X - 1, pebble.Y).team == pebble.team)
+            {
+                returnedList.Add(getPebbleByXY(pebble.X - 1, pebble.Y));
+            }
+            if (getPebbleByXY(pebble.X + 1, pebble.Y).team == pebble.team)
+            {
+                returnedList.Add(getPebbleByXY(pebble.X + 1, pebble.Y));
+            }
+            if (getPebbleByXY(pebble.X, pebble.Y + 1).team == pebble.team)
+            {
+                returnedList.Add(getPebbleByXY(pebble.X, pebble.Y+1));
+            }
+            if (getPebbleByXY(pebble.X, pebble.Y -1 ).team == pebble.team)
+            {
+                returnedList.Add(getPebbleByXY(pebble.X, pebble.Y-1));
+            }
+
+            return returnedList;
         }
     }
 }
