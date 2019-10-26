94e345cf235f36cc996f1d13e7bea08fdef7d8cd
Kolev,Nikolay N <n.kolev@student.fontys.nl>
2019-10-26 11:55:05 +0200

    Added Grid  classes

---
+++ Jigaboom/Jigaboom/Grid/Grid.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Jigaboom.Grid
+{
+    class Grid
+    {
+        List<Pebble> pebbles = new List<Pebble>();
+        
+        Pebble getPebbleByXY(int x, int y)
+        {
+            foreach(Pebble pebble in pebbles)
+            {
+                if(pebble.X == x && pebble.Y == y)
+                {
+                    return pebble;
+                }
+            }
+            return null;
+        }
+
+        List<Pebble> getAdjasentPebbles(Pebble pebble)
+        {
+            if()
+        }
+    }
+}
